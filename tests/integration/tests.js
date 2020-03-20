689f523e6807341e9ff7438243ee6ccc607cd3ce
Catherine Hall <catherine1992123@hotmail.com>
2020-03-20 09:08:36 +0000

    4 integration tests passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,31 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should update the display of the running total when the number buttons are pressed', function(){
+   running_total = element(by.css('#running_total'))
+   element(by.css('#number3')).click();
+   element(by.css('#number2')).click();
+   expect(running_total.getAttribute('value')).to.eventually.equal('32')
+  })
+
+  it('should update the display with the result of the operation when arithmetical operations are pressed', function(){
+  running_total = element(by.css('#running_total'))
+       element(by.css('#number8')).click();
+       element(by.css('#operator_multiply')).click();
+       element(by.css('#number2')).click();
+       element(by.css('#operator_equals')).click();
+       expect(running_total.getAttribute('value')).to.eventually.equal('16')
+  })
+
+  it('should be able to chain multiple operations together', function(){
+     running_total = element(by.css('#running_total'))
+     element(by.css('#number3')).click();
+     element(by.css('#operator_add')).click();
+     element(by.css('#number9')).click();
+     element(by.css('#operator_divide')).click();
+     element(by.css('#number2')).click();
+     element(by.css('#operator_equals')).click();
+     expect(running_total.getAttribute('value')).to.eventually.equal('6')
+    })
+
 });
--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -11,37 +11,37 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
-   it('it should be able to add one number to another to get the correct total', function(){
+   it('should be able to add one number to another to get the correct total', function(){
    calculator.previousTotal = 1
    calculator.add(4)
    assert.equal(5, calculator.runningTotal)
    })
 
-   it('it should be able to subtract one number from another to get the correct total', function(){
+   it('should be able to subtract one number from another to get the correct total', function(){
    calculator.previousTotal = 7
    calculator.subtract(4)
    assert.equal(3, calculator.runningTotal)
    })
 
-   it('it should be able to multiply one number by another to get the correct total', function(){
+   it('should be able to multiply one number by another to get the correct total', function(){
    calculator.previousTotal = 3
    calculator.multiply(5)
    assert.equal(15, calculator.runningTotal)
    })
 
-   it('it should be able to divide one number by another to get the correct total', function(){
+   it('should be able to divide one number by another to get the correct total', function(){
    calculator.previousTotal = 21
    calculator.divide(7)
    assert.equal(3, calculator.runningTotal)
    })
 
-   it('it should be able to click on a series of number and take these as a new number with multiple digits', function(){
+   it('should be able to click on a series of number and take these as a new number with multiple digits', function(){
    calculator.numberClick(5)
    calculator.numberClick(6)
    assert.equal(56, calculator.runningTotal)
    })
 
-   it('it should be able to chain together multiple operations', function(){
+   it('should be able to chain together multiple operations', function(){
    calculator.previousTotal = 0
    calculator.operatorClick('+')
    calculator.numberClick(2)
@@ -51,7 +51,7 @@ describe('calculator', function () {
    assert.equal(1, calculator.previousTotal)
    })
 
-   it('it should be able to clear the running total without affecting the calculation', function(){
+   it('should be able to clear the running total without affecting the calculation', function(){
    calculator.previousTotal = 0
    calculator.operatorClick('+')
    calculator.numberClick(2)
